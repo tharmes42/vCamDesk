508221f69b56cfcc261a742db04592fde01e7e61
tharmes42 <tge@harmes.de>
2020-12-05 14:38:13 +0100

    updated build script

--- VCamDeskApp/build/build_and_pack.bat
+++ VCamDeskApp/build/build_and_pack.bat
@@ -8,7 +8,7 @@ FOR /F "USEBACKQ" %%F IN (`powershell -NoLogo -NoProfile -Command ^(Get-Item "vC
 echo Determined release from vCamDesk-non-packed.exe: %fileVersion%
 
 REM merge .exe and .dll files together
-%userprofile%\.nuget\packages\ilmerge\3.0.41\tools\net452\ILMerge.exe /log /ndebug /out:vCamDesk.exe vCamDesk-non-packed.exe AForge.Controls.dll AForge.dll AForge.Imaging.dll AForge.Math.dll AForge.Video.DirectShow.dll AForge.Video.dll Newtonsoft.Json.dll nucs.JsonSettings.dll
+%userprofile%\.nuget\packages\ilmerge\3.0.41\tools\net452\ILMerge.exe /log /ndebug /out:vCamDesk.exe vCamDesk-non-packed.exe AForge.Controls.dll AForge.dll AForge.Imaging.dll AForge.Math.dll AForge.Video.DirectShow.dll AForge.Video.dll Newtonsoft.Json.dll nucs.JsonSettings.dll System.Net.Http.Formatting.dll
 
 REM zip everything
 "C:\Program Files\7-Zip\7z.exe" a -tzip vCamDesk-release-v%fileVersion%.zip vCamDesk.exe ../../../README.md
